6cea16be945e1480897dfec0b7c3203f0a893bbb
Liam Robert Atkins <liamrobertatkins@Liams-MacBook-Pro.local>
2022-05-06 08:31:28 +0100

    app and components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
-import { useState } from 'react'
-import Header from "./components/Header"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
-import Card from './components/shared/Card'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './context/FeedbackContext'
 
-function App () {
-
-  const [feedback, setFeedback] = useState
-  (FeedbackData)
-
-  const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
-
+function App() {
   return (
-    <>
-      <Header text={true} />
-      <div className="container">
-        <FeedbackList 
-          feedback={feedback} 
-          handleDelete={deleteFeedback}
-        />
-      </div>
-    </>
-  )
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
 
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  )
 }
 
 export default App
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,33 +1,44 @@
-import PropTypes from 'prop-types'
-import FeedbackItem from "./FeedbackItem"
 
-function FeedbackList({ feedback, handleDelete }) {
+import { motion, AnimatePresence } from 'framer-motion'
+import { useContext } from 'react'
+import FeedbackItem from './FeedbackItem'
+import Spinner from './shared/Spinner'
+import FeedbackContext from '../context/FeedbackContext'
 
-  if (!feedback || feedback.length === 0) {
-    return <p>No feedback</p>
+function FeedbackList() {
+  const { feedback, isLoading } = useContext(FeedbackContext)
+
+  if (!isLoading && (!feedback || feedback.length === 0)) {
+    return <p>No Feedback Yet</p>
   }
 
-  return (
-    <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem 
-          key={item.id} 
-          item={item} 
-          handleDelete={(handleDelete)} 
-        />
-      ))}
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            layout
+          >
+            <FeedbackItem key={item.id} item={item} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
-}
 
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf (
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  )
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
 export default FeedbackList
